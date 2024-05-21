d2c5274c31a2227fa9920ca572c73008c9c73d46
Code-28 <khamesh05@gmail.com>
2024-05-21 14:03:25 +0530

    Initial Commit

---
+++ Emojidisplay/EmojidisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojidisplayApp.swift
+//  Emojidisplay
+//
+//  Created by Khamesh P on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojidisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
